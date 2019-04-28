6c087a5fa23ee10d0874c914088805856e6aa0fd
vanya-black <blakx@mail.ru>
2019-04-28 11:57:32 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def wsgi_app (env, start_response):
-	body = env['QUERY_STRING'].split('&')
+	body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
 	status = '200 OK'
 	headers = [('Content-Type','text/plain')]
 	start_response(status, headers)
