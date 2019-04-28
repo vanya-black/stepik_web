478c09f4a90943499f4809ee0cd6cfc2ed2a4d93
vanya-black <blakx@mail.ru>
2019-04-28 15:31:29 +0300

    1

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn stop
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_app
-##sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_app
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
