f36b79d0e14e1f947d11602fe87fce3528303e67
vanya-black <blakx@mail.ru>
2019-04-28 15:35:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
 sudo /etc/init.d/gunicorn stop
 #sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_app
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:app
