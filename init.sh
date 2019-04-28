801cfdb1a06bea64fd0510dcfd8ae77bc5bd6b70
vanya-black <blakx@mail.ru>
2019-04-28 20:33:15 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo pip3 install --upgrade Django==2.0.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
