a95e053e5cc9a08bca7e4a2876159cc885ca2ded
vanya-black <blakx@mail.ru>
2019-04-28 11:47:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart -c /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn stop
+sudo /etc/init.d/gunicorn -c /etc/gunicorn.d/hello.py start
