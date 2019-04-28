3dc752a79286e154fee8d4c4394ecaf52176c519
vanya-black <blakx@mail.ru>
2019-04-28 11:16:52 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def wsgi_app (env, start_response):
+	body = env['QUERY_STRING'].split('&')
+	status = '200 OK'
+	headers = [('Content-Type','text/plain')]
+	start_response(status, headers)
+	return(body)
+
