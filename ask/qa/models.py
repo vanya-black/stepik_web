b9c3a651319363a0165a48fa128bfa423f25244f
vanya-black <blakx@mail.ru>
2019-05-05 16:02:37 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -16,8 +16,8 @@ class Question (models.Model):
     objects = QuestionManager() 
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True)
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name="q_to_likes")
     
