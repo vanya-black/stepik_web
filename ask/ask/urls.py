9a7ecc162ae9a492b293aa0b95abdd8ce1a1b444
vanya-black <blakx@mail.ru>
2019-05-05 23:05:34 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from qa.views import main, popular, ask
+
 urlpatterns = [
-    ##url(r'^admin/', admin.site.urls),
-    url(r'^login/', include('qa.urls')),
-    url(r'^signup/', include('qa.urls')),
-    url(r'^question\/\d+$', include('qa.urls')),
-    url(r'^ask/', include('qa.urls')),
-    url(r'^popular/', include('qa.urls')),
-    url(r'^new/', include('qa.urls')),
-    url(r'^$', include('qa.urls'))
+
+    url(r'^$', index),
+    url(r'^ask/', ask),
+    url(r'^popular/', popular),
+    url(r'^new/', found),
+    url(r'^admin/', admin.site.urls),
+    url(r'^question/', include('qa.urls')),
+    url(r'^', not_found),
 ]
