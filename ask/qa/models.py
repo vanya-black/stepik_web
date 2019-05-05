f94e4ecf90182361d23cb708d639b9d237c21c02
vanya-black <blakx@mail.ru>
2019-05-05 15:35:53 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,31 +6,31 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question (models.Model):
-	objects = QuestionManager() 
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateTimeField(blank=True)
-	rating = models.IntegerField()
-	author = models.ForeignKey(User,
-			null=True, on_delete_models.SET_NULL)
-	likes = models.ManyToManyField(User)
-	
-	def __str__(self):
-		return self.title
+    objects = QuestionManager() 
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User,
+            null=True, on_delete_models.SET_NULL)
+    likes = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return self.title
 
 class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateTimeField(blank=True)
-	questinon = models.ForeignKey(Question, null=True, on_delete_models.SET_NULL)
-	author = models.ForeignKey(User,
-			null=True, on_delete_models.SET_NULL)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    questinon = models.ForeignKey(Question, null=True, on_delete_models.SET_NULL)
+    author = models.ForeignKey(User,
+            null=True, on_delete_models.SET_NULL)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 class QuestionManager(models.Manager): 
-	def new(self):
-    	return self.order_by('-added_at')
+    def new(self):
+        return self.order_by('-added_at')
 
-	def popular(self):
-   		return self.order_by('-rating')
+    def popular(self):
+           return self.order_by('-rating')
