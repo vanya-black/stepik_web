b96f17fca73fbddfcd3276ae04274df441aa639d
vanya-black <blakx@mail.ru>
2019-05-06 13:50:16 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -10,31 +10,38 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 def question(request, id):
-	try:
-		q = Question.objects.get(id=id)
-	except Question.DoesNotExist:
-		raise Http404
-	return render(request, 'question.html', {'question': q})
+    '''
+    try:
+        q = Question.objects.get(id=id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'question.html', {'question': q})
+    '''
+    return HttpResponse("Here's the text of the Web page.")
 
 def index(request):
-	questions = Question.objects.new()
-	try:
-		page = int(request.GET.get('page'))
-	except ValueError:
-		page = 1
-	paginator = Paginator(question, 10)
-	try:
-		page = paginator.page(page)
-	except EmptyPage:
-		page = paginator.page(paginator.num_pages)
-	return render(request, 'main.html', {
-		'Title' : 'Latest',
-		'paginator' : paginator,
-		'questions' : page.object_list,
-		'page' : page
-		})
+    '''
+    questions = Question.objects.new()
+    try:
+        page = int(request.GET.get('page'))
+    except ValueError:
+        page = 1
+    paginator = Paginator(question, 10)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return render(request, 'main.html', {
+        'Title' : 'Latest',
+        'paginator' : paginator,
+        'questions' : page.object_list,
+        'page' : page
+        })
+        '''
+    return HttpResponse("Here's the text of the Web page.")
 
 def popular(request):
+    '''
     try:
         page = int(request.GET.get("page"))
     except ValueError:
@@ -48,4 +55,6 @@ def popular(request):
                    'paginator': paginator,
                    'questions': page.object_list,
                    'page': page, })
+                '''
+    return HttpResponse("Here's the text of the Web page.")
 
