7fc2bbeb34e2f372607bd572052ccc6c5dc796db
vanya-black <blakx@mail.ru>
2019-05-06 14:40:02 +0300

    1

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo pip3 install --upgrade Django==2.0.0
+sudo pip3 install --upgrade gunicorn==19.6.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
