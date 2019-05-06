4a037c2283c2fb650c1ae2d39ea479907e209404
vanya-black <blakx@mail.ru>
2019-05-06 14:24:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,5 @@ sudo pip3 install --upgrade Django==2.0.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
